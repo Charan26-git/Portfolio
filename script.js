82ab3d9142104b72797f8d53fc148375e16bcec0
Charan26-git <163013820+Charan26-git@users.noreply.github.com>
2024-07-25 18:20:26 +0530

    Create script.js
    This is the js code

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Java Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true})
